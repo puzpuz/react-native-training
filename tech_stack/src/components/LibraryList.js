b4340e050a898416c782f7ba38e56f8c255fd0aa
richard-pc\richard <richard@times10.co>
2017-02-14 19:03:21 +0800

    add methods for library list

--- tech_stack/src/components/LibraryList.js
+++ tech_stack/src/components/LibraryList.js
@@ -6,3 +6,34 @@ const LibraryList = () => (
 
   );
 export default LibraryList;
+
+/*import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ListView } from 'react-native';
+
+class LibraryList extends Component {
+ componentWillMount() {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(this.props.libraries);
+ }
+ renderRow() {
+    
+ }
+ render() {
+    return (
+      <ListView
+        dataSource={this.dataSource}
+        renderRow={this.renderRow}
+      />
+    );
+  }
+}
+const mapStateToProps = state => {
+  return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);*/
+
